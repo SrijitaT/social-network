864776b74710a7ec5c156e6e4ac1e0547ca2bbc4
SrijitaT <srijita.thakur@gmail.com>
2018-09-08 16:56:16 +0530

    commented dev tools

--- client/src/store.js
+++ client/src/store.js
@@ -9,8 +9,8 @@ const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleWare),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleWare)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 //rootreducer
