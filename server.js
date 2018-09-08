1b7b1897bba82d2ead879474dbd43f8dcd829b03
SrijitaT <srijita.thakur@gmail.com>
2018-09-08 16:12:10 +0530

    prepared for deployment

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const passport = require("passport");
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
+const path = require("path");
 
 const app = express();
 
@@ -34,6 +35,13 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+//Server static assests if in production
 
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running ${port}`));
